05a4a1158d8a33060fd0222d7472417de45fcf36
Jack <shiungchenyu@gmail.com>
2018-09-26 19:11:58 +0800

    update

---
+++ js/datatable.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+    $('#example').DataTable({
+        "dom": '<"top"i>rt<"bottom-page"p><"clear">',
+        // "lengthMenu": [[10, 20, 30, -1], [10, 20, 30, "All"]],
+        "pageLength": 5,
+        "responsive": true,
+        "bLengthChange" : false,
+        "bFilter": false,
+        "bInfo": false,
+        "ajax": 'data.json',
+        "columns": [
+            { "data": function(source, type, val){
+                var checkBox = `
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="${source.id}" value="option1">
+                </div>
+                `
+                return checkBox;
+            }},
+            { "data": "id" },
+            { "data": "owner" },
+            { "data": "insueredName" },
+            { "data": "productType" },
+            { "data": "currency" },
+            { "data": "create" },
+            { "data": "sign" },
+            { "data": "status" },
+            { "data":function(source, type, val){
+                var options = `
+                <a href="javascript:;"><i class="material-icons options">visibility</i></a>
+                <a href="javascript:;"><i class="material-icons options">print</i></a>
+                `
+                return options;
+            }}
+        ],
+        "columnDefs": [
+            { "orderable": false, "targets": [0,1,2,3,4,5,9] }
+        ],
+        "order": [],
+        // "pagingType": "simple_numbers",
+        "language":{
+            paginate:
+                {
+                    previous: "<",
+                    next: ">",
+                    first: "|<",
+                    last: ">|"
+                }
+        }
+    });
+
+    $('#example2').DataTable({
+        "dom": '<"top"i>rt<"bottom-page"p><"clear">',
+        // "lengthMenu": [[10, 20, 30, -1], [10, 20, 30, "All"]],
+        "pageLength": 5,
+        "responsive": true,
+        "bLengthChange" : false,
+        "bFilter": false,
+        "bInfo": false,
+        "ajax": 'data.json',
+        "columns": [
+            { "data": "id" },
+            { "data": "owner" },
+            { "data": "insueredName" },
+            { "data": "productType" },
+            { "data": "currency" },
+            { "data": "create" },
+            { "data": "sign" },
+            { "data": "status" },
+            { "data":function(source, type, val){
+                var options = `
+                <a href="javascript:;"><i class="material-icons options">visibility</i></a>
+                <a href="javascript:;"><i class="material-icons options">print</i></a>
+                <a href="javascript:;"><i class="material-icons options">edit</i></a>
+                <a href="javascript:;"><i class="material-icons options">delete</i></a>
+                `
+                return options;
+            }}
+        ],
+        "columnDefs": [
+            { "orderable": false, "targets": [0,1,2,3,4,8] }
+        ],
+        "order": [],
+        // "pagingType": "simple_numbers",
+        "language":{
+            paginate:
+                {
+                    previous: "<",
+                    next: ">",
+                    first: "|<",
+                    last: ">|"
+                }
+        }
+    });
+});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
+
+
+    
+    $('.collapse').on('show.bs.collapse',function(){   
+        var collapseSelect = $(this).parents('.accordion');
+        collapseSelect.find('button.btn-link').attr('data-toggle','collapse');
+        $(this).parents('.card').find('button.btn-link').attr('data-toggle',null);
+        stepProccess(collapseSelect,$(this));
+    }) 
+
+    $(".dropdown-item").on('click',function(){
+        var dropdownSelect = $(this).parents('.dropdown').find('button.dropdown-toggle');
+        dropdownSelect.siblings('.dropdown-menu').find('.dropdown-item').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $(".circle").on('click',function(){
+        var currentID =  $(this).parent().attr('class');
+        if(!currentID.match('active')){
+            $("#"+currentID).collapse('show');
+        }
+    })
+
+    $("#phone").on('input',function(){
+        var result = universal['checkPhone']($(this).val());
+        if(result){
+            $(this).siblings("i.material-icons").text("check_circle");
+            $(this).siblings("i.material-icons").removeClass('text-danger');
+            $(this).siblings("i.material-icons").addClass('active text-success'); 
+        }else{
+            $(this).siblings("i.material-icons").text("error");
+            $(this).siblings("i.material-icons").removeClass('text-success');
+            $(this).siblings("i.material-icons").addClass('active text-danger');
+        }
+        errorInfo($(this),result);
+    });
+
 });
+
+
+// step-proccess
+function stepProccess(area,target){
+    area.siblings('.step-process').find('[class^="step-process-"]').removeClass('active');
+    var currentID = target.attr('id');
+    $("."+currentID).addClass('active');
+}
+
+//驗證
+(obj => {
+    typeof module === 'object' && module.exports ? module.exports = obj : this[obj.name] = obj
+})({
+    name: 'universal', // 前端 <script> 標籤的全域名稱
+    checkEmail: val => /.+@.+\..+/.test(val),
+    checkPassword (val) {
+        return /.{8,}/.test(this._password = val)
+    },
+    checkConfirmPassword (val) {
+        return this._password === val
+    },
+    checkPhone: val => /^\d{5,}$/.test(val),
+    checkAddress: val => /[\u4e00-\u9fa5]/.test(val),
+    checkImage: ary => ary.length <= 3 && ary.every(file => file.width <= 150 || file.height <= 150),
+    checkCardNumber (val) {
+        val = val.replace(/\s/g, '')
+        return (/^4[0-9]{12}(?:[0-9]{3})?$/.test(val) && 'visa') ||
+        (/^5[1-5][0-9]{14}$/.test(val) && 'master') || ''
+    }
+})
+
+// 驗證 info
+function errorInfo(val,status){
+    if(status){
+        val.siblings('.error-info').hide();
+    }else{
+        val.siblings('.error-info').show();
+    }
+    
+}
